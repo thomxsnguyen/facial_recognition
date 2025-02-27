60abeb18ed79a0cdf97b02f361ef9885e43b7e25
Thomas <thomasn8255@gmail.com>
2025-02-26 19:44:24 -0800

    first implementation of facial tracker

---
+++ Desktop/facial_recognition/build/CMakeFiles/FaceDetection.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FaceDetection.dir/facedetect.cpp.o"
+  "CMakeFiles/FaceDetection.dir/facedetect.cpp.o.d"
+  "FaceDetection"
+  "FaceDetection.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FaceDetection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
